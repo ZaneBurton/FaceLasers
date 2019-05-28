84c588aeb7f2e3fcd0db0b3b45f7afaf17d258cf
Zane Burton <zaneburton@zanes-macbook-air.local>
2019-05-28 09:12:08 -0500

    Initial Commit

---
+++ FaceLasers/FaceDetectionViewController.swift
@@ -0,0 +1,129 @@
+/// Copyright (c) 2019 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import AVFoundation
+import UIKit
+import Vision
+
+class FaceDetectionViewController: UIViewController {
+  
+
+  
+  @IBOutlet var faceView: FaceView!
+  @IBOutlet var laserView: LaserView!
+  @IBOutlet var faceLaserLabel: UILabel!
+  
+  let session = AVCaptureSession()
+  var previewLayer: AVCaptureVideoPreviewLayer!
+  
+  let dataOutputQueue = DispatchQueue(
+    label: "video data queue",
+    qos: .userInitiated,
+    attributes: [],
+    autoreleaseFrequency: .workItem)
+
+  var faceViewHidden = false
+  
+  var maxX: CGFloat = 0.0
+  var midY: CGFloat = 0.0
+  var maxY: CGFloat = 0.0
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureCaptureSession()
+    
+    laserView.isHidden = true
+    
+    maxX = view.bounds.maxX
+    midY = view.bounds.midY
+    maxY = view.bounds.maxY
+    
+    session.startRunning()
+  }
+}
+
+// MARK: - Gesture methods
+
+extension FaceDetectionViewController {
+  @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+    faceView.isHidden.toggle()
+    laserView.isHidden.toggle()
+    faceViewHidden = faceView.isHidden
+    
+    if faceViewHidden {
+      faceLaserLabel.text = "Lasers"
+    } else {
+      faceLaserLabel.text = "Face"
+    }
+  }
+}
+
+// MARK: - Video Processing methods
+
+extension FaceDetectionViewController {
+  func configureCaptureSession() {
+    // Define the capture device we want to use
+    guard let camera = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                               for: .video,
+                                               position: .front) else {
+      fatalError("No front video camera available")
+    }
+    
+    // Connect the camera to the capture session input
+    do {
+      let cameraInput = try AVCaptureDeviceInput(device: camera)
+      session.addInput(cameraInput)
+    } catch {
+      fatalError(error.localizedDescription)
+    }
+    
+    // Create the video data output
+    let videoOutput = AVCaptureVideoDataOutput()
+    videoOutput.setSampleBufferDelegate(self, queue: dataOutputQueue)
+    videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+    
+    // Add the video output to the capture session
+    session.addOutput(videoOutput)
+    
+    let videoConnection = videoOutput.connection(with: .video)
+    videoConnection?.videoOrientation = .portrait
+    
+    // Configure the preview layer
+    previewLayer = AVCaptureVideoPreviewLayer(session: session)
+    previewLayer.videoGravity = .resizeAspectFill
+    previewLayer.frame = view.bounds
+    view.layer.insertSublayer(previewLayer, at: 0)
+  }
+}
+
+// MARK: - AVCaptureVideoDataOutputSampleBufferDelegate methods
+
+extension FaceDetectionViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+  func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+  }
+}
